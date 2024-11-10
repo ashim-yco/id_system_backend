0ba65d39777151de283673b98ef0de961323d3b3
Ashim <ashim.baral50@gmail.com>
2024-11-10 09:44:26 +0545

    feat: implement url path to ge all users' details

--- authuser/api/urls/urls.py
+++ authuser/api/urls/urls.py
@@ -4,6 +4,7 @@ from authuser.api.views import user_view
 urlpatterns = [
     path("registeruser/", user_view.RegisterUserView.as_view(), name="RegisterUser"),
     path("loginuser/", user_view.LoginUserView.as_view(), name="LoginUser"),
+    path("getallusers/", user_view.AllUsersResponseView.as_view(), name="GetAlUsers"),
     path(
         "getuserdetails/<int:userid>",
         user_view.UserDetailView.as_view(),
